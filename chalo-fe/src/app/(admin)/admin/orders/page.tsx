d65bc0298c8064e1a14641b5125f202b39074a6c
chauip's Gol <chaupt2823@gmail.com>
2026-08-15 14:32:03 +0700

    refactor: host prep dock in admin layout

--- chalo-fe/src/app/(admin)/admin/orders/page.tsx
+++ chalo-fe/src/app/(admin)/admin/orders/page.tsx
@@ -11,7 +11,6 @@ import { useSearchParams } from "next/navigation";
 import { AdminMobilePageHeader } from "../../_components/AdminMobilePageHeader";
 import AdminOrdersHistory from "./_components/AdminOrdersHistory";
 import { AdminOrdersModeSwitch } from "./_components/AdminOrdersModeSwitch";
-import { AdminOrdersOperationsLayout } from "./_components/AdminOrdersOperationsLayout";
 
 const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:8080/api";
 
@@ -49,7 +48,7 @@ export default function AdminOrdersPage() {
         <AdminMobilePageHeader title="Đơn hàng" description={isHistory ? "Lịch sử đơn hàng" : "Bảng vận hành realtime"} />
         <div className="mt-3"><AdminOrdersModeSwitch /></div>
       </div>
-      {isHistory ? <AdminOrdersHistory /> : <AdminOrdersOperationsLayout board={<AdminOrdersOperations />} />}
+      {isHistory ? <AdminOrdersHistory /> : <AdminOrdersOperations />}
     </div>
   );
 }
