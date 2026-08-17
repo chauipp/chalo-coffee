05058af20be7f9225ddcfc9b51c3f0d00ee772a0
chauip's Gol <chaupt2823@gmail.com>
2026-08-17 19:09:42 +0700

    feat(be): phản ánh hoàn tiền trong báo cáo ca

--- chalo-be/src/modules/shift/shift.module.ts
+++ chalo-be/src/modules/shift/shift.module.ts
@@ -4,8 +4,9 @@ import { CashShift } from './entities/cash-shift.entity';
 import { PaymentTransaction } from '../payment/entities/payment-transaction.entity';
 import { PaymentAllocation } from '../payment/entities/payment-allocation.entity';
 import { Order } from '../order/entities/order.entity';
+import { RefundTransaction } from '../payment/entities/refund-transaction.entity';
 import { ShiftService } from './shift.service';
 import { ShiftController } from './shift.controller';
 
-@Module({ imports: [TypeOrmModule.forFeature([CashShift, PaymentTransaction, PaymentAllocation, Order])], providers: [ShiftService], controllers: [ShiftController], exports: [ShiftService] })
+@Module({ imports: [TypeOrmModule.forFeature([CashShift, PaymentTransaction, PaymentAllocation, RefundTransaction, Order])], providers: [ShiftService], controllers: [ShiftController], exports: [ShiftService] })
 export class ShiftModule {}
